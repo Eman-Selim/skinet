fe34f43b6d7be8f3c6c760a69378500d9ac200b0
unknown <emanselim305@gmail.com>
2023-01-05 16:02:20 +0200

    dvgdf

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using Infrastructure.Data;
 using core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using core.Interfaces;
 
 namespace API.Controllers
 {
@@ -13,22 +14,22 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController:ControllerBase
     {
-        private readonly StoreContext _context;
+        private readonly IProductRepository _repo;
 
-        public ProductsController(StoreContext context)
-        {
-            _context = context;
-        }
+            public ProductsController(IProductRepository repo)
+            {
+                _repo = repo;
+            }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts(){
-            var product= await _context.Products.ToListAsync();
+            var product= await _repo.GetProductsAsync();
             return Ok(product);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id){
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
         }
     }
 }
